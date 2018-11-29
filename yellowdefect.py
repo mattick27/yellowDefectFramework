883415f4548610bbef52f4b4190c02eac14ea5c3
mattck27 <Matichon3149@gmail.com>
2018-11-29 19:59:27 +0700

    add report excel format

--- yellowdefect.py
+++ yellowdefect.py
@@ -10,10 +10,12 @@ import matplotlib
 import pandas as pd 
 tolList=[1e-4,1e-3,1e-2,1e-1,1e0,1e1,1e2,1e3,1e4]
 CList=[1e-4,1e-3,1e-2,1e-1,1e0,1e1,1e2,1e3,1e4]
-modelList = {'0': LinearSVC , '1' : LogisticRegression}    
+modelList = {'0': LinearSVC , '1' : LogisticRegression}
+
 def buildModel(x_Train,y_Train,model='All'):
     if model == 'All':
         arrList = []
+        modelName = []
         for idx in range(2) : 
             print("Model's order is {}".format(modelList[str(idx)].__name__))
             arr = []
@@ -23,8 +25,9 @@ def buildModel(x_Train,y_Train,model='All'):
                     clf = clf.fit(x_Train,y_Train)
                     arr.append(clf)
                     clf = 0
+                    modelName.append('{} C = {}, tol = {}'.format(modelList[str(idx)].__name__,C,tol))
             arrList.append(np.array(arr))
-        return np.array(arrList) 
+        return (np.array(arrList) , modelName)
     elif model == 'Logistic':
         pass
     elif model == 'SVC':
@@ -37,7 +40,7 @@ def validateModel(modelList,x_validation,y_validation):
             pred = i.predict(x_validation)
             confusion = confusion_matrix(pred,y_validation)
             acc = accuracy_score(pred,y_validation)
-            preRe = precision_recall_fscore_support(pred,y_validation,average=None)
+            preRe = precision_recall_fscore_support(pred,y_validation,average='macro')
             table[idx].append(
                 {
                 'confusion' : np.array(confusion),
@@ -47,8 +50,7 @@ def validateModel(modelList,x_validation,y_validation):
             )
     return np.array(table)
 
-def reportTheReport(data,mode='overview'):
-    print(data.shape)
+def reportTheReport(data,modelName = None ,mode='overview'):
     if mode == 'overview':
         dictList = {'Model' : []}
         for idxModel,unpackData in enumerate(data) : 
@@ -74,23 +76,26 @@ def reportTheReport(data,mode='overview'):
                             dictList['recall{}'.format(numIndex)] = [i]
                             numIndex+= 1 
                 dictList['Model'].append(modelList[str(idxModel)].__name__)
+        if modelName : 
+            dictList['Model'] = modelName
         df = pd.DataFrame(dictList)
         writer = pd.ExcelWriter("test.xlsx")
-        df.to_excel(writer)
+        df.to_excel(writer,'{}'.format(idxModel))
     if mode == 'confusion':
-        for unpackData in data : 
-            for IteData in unpackData:         
-                print('--------------------')       
-                print(IteData['confusion'])
-                print('--------------------')
-                pass
-
+        for idx,unpackData in enumerate(data) : 
+            for idxData,IteData in enumerate(unpackData): 
+                if modelName : 
+                    numIndex = int(idx*len(unpackData))+int(idxData)
+                    print('----- {} -------'.format(modelName[numIndex]))       
+                    print(IteData['confusion'])
+                    pass
+                else : 
+                    pass
 
 iris = datasets.load_iris()
 x = iris.data[:,:]  # we only take the first two features.
 y = iris.target
 
-arr = buildModel(x,y,'All')
+arr,modelName = buildModel(x,y,'All')
 data = validateModel(arr,x,y)
-reportTheReport(data = data)
-
+reportTheReport(modelName = modelName,data = data,mode='overview')
